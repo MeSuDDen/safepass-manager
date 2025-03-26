8947ff07312b4aceac7b406821b9d4f1c9a88c92
Эльвира Григорьева <workdev.sudden@gmail.com>
2025-03-26 16:13:49 +1000

    Created dockerfiles for back&front & docker-compose

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b1gh0l2u0o8"
+    "revision": "0.joaoan8im6"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
